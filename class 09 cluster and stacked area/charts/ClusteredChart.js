a251e6e42b3887a7de43c5e80d503e298401208e
Kacper Agatowski <n00212272@iadt.ie>
2023-03-10 10:56:55 +0000

    deleted piechart and added cluster and area

---
+++ class 09 cluster and stacked area/charts/ClusteredChart.js
@@ -0,0 +1,143 @@
+//passing a parameter and calling this height to whatever the height is placed
+class ClusteredChart {
+  constructor(_height, _width, _posX, _posY, _data, _numTicks,_title,_yValue,_yValueTitle,_xValue1,_xValue2,_xValue3,_xValue4){
+    this.height =  _height;
+    this.width =  _width;
+    this.posX =  _posX;
+    this.posY =  _posY;
+    this.data =  _data;
+    this.margin = 20;
+    this.numTicks = _numTicks;
+    this.dataGap = 60;
+    this.xValue1 = _xValue1;
+    this.xValue2 = _xValue2;
+    this.xValue3= _xValue3;
+    this.xValue4 = _xValue4;
+    this.yValue = _yValue;
+    this.yValueTitle =_yValueTitle;
+    this.title = _title;
+    this.dataArray = [_xValue1,_xValue2,_xValue3,_xValue4]
+    
+    
+    this.highestNum = this.maximumValue();
+   
+
+    
+  }
+  
+  render(){
+      push();
+      translate(this.posX, this.posY);
+      this.drawXAxis();
+      this.drawYAxis();
+      this.drawCluster();
+        pop();
+  }
+  maximumValue(){
+     let highest = 0;
+      for(let x=0; x<this.data.getRowCount(); x++){
+        if(int(data.rows[x].obj[this.xValue1])>highest){
+          highest=int(data.rows[x].obj[this.xValue1]);        
+        }
+  }
+      //managing the rounding
+      for (let x=highest; x<1000000;x++){
+        if(x%this.numTicks==0 && x%10==0){
+          highest = x;
+          break;
+        }
+      }
+      return highest;
+    }
+   
+   
+  dataScaled(_num,_num2){
+      let dataScaler = this.highestNum/this.height
+       return _num / dataScaler
+       
+       
+ }
+
+ drawXAxis(){
+
+  line(0,0,this.width,0);
+
+  let dataWidth =( this.width - (this.margin*2) - (this.data.getRowCount()-1)* this.dataGap) / this.data.getRowCount();
+  let dataUnit = dataWidth + this.dataGap;
+
+  push();
+  translate(this.margin,0);
+  let labels= this.data.getColumn(this.yValue);
+  for(let x=0; x<labels.length; x++){
+    let value = labels[x];
+    fill(0);
+    textSize(20);
+    textAlign(CENTER);
+    text(value,x*dataUnit + (dataWidth/2),20);
+  }
+  textSize(30);
+  text(this.yValue,this.width/2,60)
+  pop()
+  textAlign(CENTER)
+  textSize(50)
+  text(this.title,this.width/2,-this.height-this.margin)
+ }
+
+ drawYAxis(){
+  line(0,0,0,-this.height);
+  for (let x=0; x<this.numTicks+1 ; x++){
+    //Y axis Line
+    stroke(50);
+    let tickWidth = this.height/this.numTicks
+    line(0,-tickWidth*x ,-this.margin ,-tickWidth*x);
+    //text
+    let textGap = this.highestNum/this.numTicks;
+    noStroke();
+    fill(50);
+    textSize(20);
+    textAlign(RIGHT,CENTER);
+    text((textGap * x).toFixed(0), -this.margin, -tickWidth * x);
+    textSize(20);
+    textWrap(WORD);
+    text(this.yValueTitle,-this.margin,-this.height/2,-this.margin*2)
+    }
+ }
+
+ drawCluster(){
+  
+  let dataWidth =( this.width - (this.margin*2) - (this.data.getRowCount()-1)* this.dataGap) / this.data.getRowCount() 
+ 
+  
+  let dataUnit = dataWidth + this.dataGap ;
+  
+
+  push()
+  translate(this.margin);  
+  
+  for(let y=0; y<this.dataArray.length;y++){
+    let ary = this.dataArray[y];
+  
+ for(let x=0;x<this.data.getRowCount();x++){
+        noStroke();
+        let clus = int(-this.data.rows[x].obj[ary])          
+          fill(0,0,255)
+          rect(x*dataUnit+this.margin,0,dataWidth,this.dataScaled(clus));
+          
+        }
+        
+      
+  }  
+  pop()
+  }
+ 
+  
+  }
+
+
+
+  
+    
+
+   
+
+
--- class 09 cluster and stacked area/sketch.js
+++ class 09 cluster and stacked area/sketch.js
@@ -2,31 +2,18 @@
 // let fruits = [23, 56, 100, 80 ,45]
 // let dogs = [23, 56, 100, 80 ,45]
 let data;
-
+let clusData;
 let charts = [];
-
 // objVal = Object.values(data.getObject());
 
 
 
 function preload() {
   data = loadTable('data/SucideStat.csv','csv','header');
+
+
 }
 
-// function tidyData(){
-//   for(let x=0; x<table.getRowCount();x++){
-//       data.push(table.rows[x].obj.Female);
-//   }
-// }
-
-// function mapData(){
-//   mappedData = [];
-//   let maxValue = max(data)
-//   for(let x = 0; x<data.length;x++){
-//       mappedData.push(map(0,data[x].Sales,maxValue,0,chartHeight))
-//   }
-//   // console.log(mappedData);
-// }
 
 
 
@@ -41,25 +28,26 @@ function setup(){
     // } 
     noLoop();
 
+
     charts.push(new BarChart(500,600,200,600, data, 5,'Year','Total','Number of Total deaths','BarChart Total Suicides in Ireland '))
     //The stacked chart is able to stack from one to 4 data sets.
     charts.push(new StackedChart(500,800,1200,600, data, 12,'Stacked Suicides in Ireland (Males & Females)','Year','Number of male and female death','Male','Female'))
 
     charts.push(new HorizontalChart(500,800,200,1700, data, 5,'Year','Total','Number of Total deaths','HorizontalChart Total suicides in Irleand'));
-    
-    // charts.push(new BarChart(600,800,100,450, dogs, 5))
 
-    // //creating an array, where i create a loop
-    // for (let x=0; x<100; x++){
-    //   let _randomNum = Math.floor(random(0,400))
-    //   charts.push(new BarChart(_randomNum));
-    // }
+    charts.push(new ClusteredChart(500,800,1200,1500, data, 12,'Clustered Suicides in Ireland (Males & Females)','Year','Number of male and female death','Male','Female'))
+
+    charts.push(new StackedArea(500,800,1200,1500, data, 12,'Stacked Suicides in Ireland (Males & Females)','Year','Number of male and female death','Male','Female'))
+    
+  
 }
 
 function draw(){
+  background(100)
   charts[0].render();
   charts[1].render();
   charts[2].render();
+  charts[3].render();
 
 
 }

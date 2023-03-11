837c6731bae047979b5029b11dbe2161189cabed
Kacper Agatowski <n00212272@iadt.ie>
2023-03-11 01:50:22 +0000

    cluster added

--- class 09 cluster and stacked area/sketch.js
+++ class 09 cluster and stacked area/sketch.js
@@ -19,7 +19,7 @@ function preload() {
 
 function setup(){
   
-    createCanvas(5000,5000);
+    createCanvas(10000,10000);
     angleMode(DEGREES);
    
     // let dataArray = data.getArray();
@@ -38,7 +38,7 @@ function setup(){
       'Number of Total deaths',
       'BarChart Total Suicides in Ireland '))
     //The stacked chart is able to stack from one to 4 data sets.
-    charts.push(new StackedChart(500,800,1200,600,
+    charts.push(new StackedChart(500,800,2400,1500,
        data, 
        12,
        'Stacked Suicides in Ireland (Males & Females)',
@@ -51,40 +51,47 @@ function setup(){
         'Year',
         'Total',
         'Number of Total deaths',
-        'HorizontalChart Total suicides in Irleand'));
+        'HorizontalChart Total suicides in Ireland'));
 
     charts.push(new ClusteredChart(500,800,1200,1500,
-       data, 
-       12,
-       'Clustered Suicides in Ireland (Males & Females)',
-       'Year',
-       'Number of male and female death',
-       'Male','Female'))
+      data, 
+      12,
+      'Clustered Suicides in Ireland (Males & Females)',
+      'Year',
+      'Number of male, Females and deaths',
+      'Total',
+      ['Male','Female','Total']))
 
-    charts.push(new StackedArea(500,800,1200,1500,
-       data, 
-       12,
-       'Stacked Suicides in Ireland (Males & Females)',
-       'Year',
-       'Number of male and female death',
-       'Male','Female'))
 
-       charts.push(new StackedChart2(500,800,1200,1500,
+       charts.push(new StackedChart2(500,800,2400,600,
         data, 
         12,
         'Stacked Suicides in Ireland (Males & Females)',
         'Year',
-        'Number of male and female death',
-        'Male','Female'))
+        'Number of male and female deaths',
+        'Total',
+        ['Male','Female']))
+
+        charts.push(new BarChart(
+          500,600,1200,600, 
+          data, 
+          5,
+          'Year',
+          'Female',
+          'Number of Female deaths',
+          'BarChart Female Suicides in Ireland '))
   
 }
 
 function draw(){
   background(200)
   charts[0].render();
-  charts[1].render();
+  charts[1].render(); //This the chart which was not looped but has an average line
   charts[2].render();
+  charts[3].render();
+  charts[4].render();
   charts[5].render();
+  // charts[5].render();
 
 
 }
